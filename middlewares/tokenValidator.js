af97251303d0dd0dc4c34d7a81394170ecdd1ee0
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-01-29 21:31:36 -0500

    listo

--- middlewares/tokenValidator.js
+++ middlewares/tokenValidator.js
@@ -9,7 +9,7 @@ const validateJWT = async (req, res, next) => {
   if (authorization && authorization.startsWith("Bearer")) {
     try {
       token = authorization.split(" ")[1];
-      console.log(token);
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select(
